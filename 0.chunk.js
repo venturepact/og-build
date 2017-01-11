c19794c5debc8e2f926a36ee7cd0e526d14be092
Naman Kareer <naman.kareer@venturepact.com>
2017-01-11 17:07:21 +0530

    Build at 01-11-2017 at 17.07.21

--- 0.chunk.js
+++ 0.chunk.js
@@ -12163,6 +12163,8 @@ var ComponentManagerComponent = (function () {
         // this.leadSection = this.getLeadSection(page);
         //toggleleadform
         this.leadSection.visible = !this.leadSection.visible;
+        console.log('>>>>>>>', this.leadSection.items[0]);
+        console.log('>>>>>>>', this.leadSection.items);
         if (this.leadSection.items[0])
             this.leadSection.items[0].visible = !this.leadSection.items[0].visible;
         if (this.leadSection.visible && this.leadSection.items[0].visible) {
