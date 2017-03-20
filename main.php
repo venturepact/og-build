d0f235fb4c412d0339e2944e3c1ccd5434d92ffb
Naman Kareer <naman.kareer@venturepact.com>
2017-03-20 14:01:50 +0530

    ogdev  Build at 03-20-2017 at 14.01.50

--- main.php
+++ main.php
@@ -21,7 +21,8 @@
 	    {
 	    	// This is the URL you want to shorten
 	    	unset($curl);
-			$longUrl = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/seo'.$_SERVER['REQUEST_URI'];
+			$name = explode("?", $_SERVER['REQUEST_URI']);
+			$longUrl = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/seo'.$name[0];
 			$apiKey = 'AIzaSyAyEiPl1ZWGqIjhCb4hPz34HgwLS_G9zZk';
 			$postData = array('longUrl' => $longUrl, 'key' => $apiKey);
 			$jsonData = json_encode($postData);
