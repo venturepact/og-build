0efed89b4a5d48cb25bdd6cfc7490352512d0baf
Tarun Gupta <soul.tarun@gmail.com>
2017-03-23 17:57:49 +0530

    Build at 03-23-2017 at 17.57.48

--- assets/js/marketing.js
+++ assets/js/marketing.js
@@ -1,11 +1,3 @@
-if (window.location.host === "app.rely.co") {
-	!function(e,l,v,i,o,n){e[i]||(e[i]={}),e[i].account_id=n;var g,h;g=l.createElement(v),g.type="text/javascript",g.async=1,g.src=o+n,h=l.getElementsByTagName(v)[0],h.parentNode.insertBefore(g,h)}(window,document,"script","_elev","https://cdn.elev.io/sdk/bootloader/v4/elevio-bootloader.js?cid=","58cfba6c892fc");
-}
-/*
- * For passing user information, please see
- * https://elev.io/api
- */
-
 _linkedin_data_partner_id = "35384";
 (function(){var s = document.getElementsByTagName("script")[0];
 var b = document.createElement("script");
