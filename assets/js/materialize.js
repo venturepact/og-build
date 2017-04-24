6a14f5930ff79857753162a16915f096ad41b5bb
Sahil Verma <sahil.verma@venturepact.com>
2017-04-24 13:07:02 +0530

    Build at 04-24-2017 at 13.07.02

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -2926,7 +2926,7 @@ if (jQuery) {
 
     // Function to update labels of text fields
     Materialize.updateTextFields = function() {
-      var input_selector = 'input[type=text], input[type=password], input[type=email],input[type=fullName],input[type=firstName],input[type=lastName],input[type=others], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
+      var input_selector = 'input[type=text], input[type=password], input[type=email],input[type=fullName],input[type=firstName],input[type=lastName],input[type=others], input[type=url], input[type=tel], input[type=tel_us], input[type=number], input[type=search], textarea';
       $(input_selector).each(function(index, element) {
         var $this = $(this);
         if ($(element).val().length > 0 || element.autofocus || $this.attr('placeholder') !== undefined) {
@@ -2940,7 +2940,7 @@ if (jQuery) {
     };
 
     // Text based inputs
-    var input_selector = 'input[type=text], input[type=password], input[type=email],input[type=fullName],input[type=firstName],input[type=lastName],input[type=others], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
+    var input_selector = 'input[type=text], input[type=password], input[type=email],input[type=fullName],input[type=firstName],input[type=lastName],input[type=others], input[type=url], input[type=tel],  input[type=tel_us], input[type=number], input[type=search], textarea';
 
     // Add active if form auto complete
     $(document).on('change', input_selector, function () {
