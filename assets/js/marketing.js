ad868154bd0b7c1b57fd3399e885ce3a5b213c45
Nitesh Kumar Gupta <nitesh.gupta@venturepact.com>
2017-09-20 17:03:42 +0530

    Build at 09-20-2017 at 17.03.42

--- assets/js/marketing.js
+++ assets/js/marketing.js
@@ -42,10 +42,20 @@ if (urla.toLowerCase().indexOf("outgrow.co") < 0) { //For .us and local
 	};
 }
 
+"https://cdn.logrocket.com/LogRocket.min.js"
+
+var logrocket = document.createElement('script');
+logrocket.setAttribute('src', "https://cdn.logrocket.com/LogRocket.min.js");
+logrocket.onload = function () {
+	window.LogRocket && window.LogRocket.init('rijgz0/outgrow');
+};
+
 if (
 	urla.toLowerCase().indexOf("outgrow.co") >= 0
 ) { //For .co
 	//console.log('Got in else');
+
+	document.getElementsByTagName('body')[0].append(logrocket);
 	var trackerProp = {
 		'ga':"UA-85018378-1",
 		'kissmetrics':"0a75a54b2fbeb2fefd728e55b0dc6bff06580cb8",
@@ -83,20 +93,3 @@ if (
 	}
 	qp('track', 'ViewContent');
 }
-
-// Mixpanel
-//
-// (function(e,a){if(!a.__SV){var b=window;try{var c,l,i,j=b.location,g=j.hash;c=function(a,b){return(l=a.match(RegExp(b+"=([^&]*)")))?l[1]:null};g&&c(g,"state")&&(i=JSON.parse(decodeURIComponent(c(g,"state"))),"mpeditor"===i.action&&(b.sessionStorage.setItem("_mpcehash",g),history.replaceState(i.desiredHash||"",e.title,j.pathname+j.search)))}catch(m){}var k,h;window.mixpanel=a;a._i=[];a.init=function(b,c,f){function e(b,a){var c=a.split(".");2==c.length&&(b=b[c[0]],a=c[1]);b[a]=function(){b.push([a].concat(Array.prototype.slice.call(arguments,0)))}}var d=a;"undefined"!==typeof f?d=a[f]=[]:f="mixpanel";d.people=d.people||[];d.toString=function(b){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);b||(a+=" (stub)");return a};d.people.toString=function(){return d.toString(1)+".people (stub)"};k="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");for(h=0;h<k.length;h++)e(d,k[h]);a._i.push([b,c,f])};a.__SV=1.2;b=e.createElement("script");b.type="text/javascript";b.async=!0;b.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";c=e.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c)}})(document,window.mixpanel||[]);mixpanel.init(trackerProp.mixpanel);
-//
-// var _kmq = _kmq || [];
-// var _kmk = _kmk || trackerProp.kissmetrics;
-// function _kms(u){
-//   setTimeout(function(){
-// 	var d = document, f = d.getElementsByTagName('script')[0],
-// 	s = d.createElement('script');
-// 	s.type = 'text/javascript'; s.async = true; s.src = u;
-// 	f.parentNode.insertBefore(s, f);
-//   }, 1);
-// }
-// _kms('//i.kissmetrics.com/i.js');
-// _kms('//scripts.kissmetrics.com/' + _kmk + '.2.js');
