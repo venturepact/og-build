529c762e0330b56f3854c87ebaff855ff44d6749
Naman Kareer <naman.kareer@venturepact.com>
2018-09-06 10:42:40 +0530

    Update main.php

--- main.php
+++ main.php
@@ -115,8 +115,8 @@
 	<body>
 		<?php
 		echo $_SERVER['HTTP_USER_AGENT'];
-		$browser = get_browser();
-		print_r($browser);
+		//$browser = get_browser();
+		//print_r($browser);
 		?>
 	</body>
 </html>
