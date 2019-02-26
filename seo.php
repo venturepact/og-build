011de9bc5e0057edc3e083b9e9dbdaf001955068
Naman Kareer <naman.kareer@venturepact.com>
2019-02-26 11:44:53 +0530

    Update seo.php

--- seo.php
+++ seo.php
@@ -36,13 +36,13 @@ if(!empty($response['data'])) {
 } else {
 ?>
 	<title>Outgrow</title>
-	<meta name="description" content="Easily create interactive experiences that increase conversion and bring traffic 123.">
-	<meta property="og:title" content="Interactive Calculators & Quizzes from Outgrow 123" />
-	<meta property="og:description" content="Easily create interactive experiences that increase conversion and bring traffic 123." />
+	<meta name="description" content="Easily create interactive experiences that increase conversion and bring traffic.">
+	<meta property="og:title" content="Interactive Calculators & Quizzes from Outgrow" />
+	<meta property="og:description" content="Easily create interactive experiences that increase conversion and bring traffic." />
 	<meta property="og:image" content="https://cdn.filestackcontent.com/1lm3eyhnQ8Kfh2li09Kq"/>
 	<meta name="twitter:card" content="summary">
-	<meta name="twitter:title" content="Interactive Calculators & Quizzes from Outgrow 123">
-	<meta name="twitter:description" content="Easily create interactive experiences that increase conversion and bring traffic 123.">
+	<meta name="twitter:title" content="Interactive Calculators & Quizzes from Outgrow">
+	<meta name="twitter:description" content="Easily create interactive experiences that increase conversion and bring traffic.">
 	<meta name="twitter:image" content="https://cdn.filestackcontent.com/1lm3eyhnQ8Kfh2li09Kq">
 <?php
 }
